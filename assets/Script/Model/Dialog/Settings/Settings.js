b3b0f1fcf3a7dd80d73f1ee47d9968c99ea12a21
buma <bumaya1024@163.com>
2024-01-25 17:41:32 +0800

    update setting

--- assets/Script/Model/Dialog/Settings/Settings.js
+++ assets/Script/Model/Dialog/Settings/Settings.js
@@ -5,59 +5,84 @@ cc.Class({
 	extends: cc.Component,
 
 	properties: {
-		NhacNen:  CkeckOut,
-		NhacGame: CkeckOut,
+		NhacNen: CkeckOut,//背景音乐
+		NhacGame: CkeckOut,//游戏音乐
+		LanguageSel: cc.ToggleContainer
+
 	},
-	onLoad(){
-		if (!cc.RedT.isSoundBackground()) {
+	onLoad()
+	{
+		if (!cc.RedT.isSoundBackground())
+		{
 			this.NhacNen.OnChangerClick();
 		}
-		if (!cc.RedT.isSoundGame()) {
+		if (!cc.RedT.isSoundGame())
+		{
 			this.NhacGame.OnChangerClick();
 		}
 	},
-	onEnable: function () {
-        this.node.runAction(cc.RedT.inGame.dialog.actionShow);
-    },
-    onDisable: function () {
-        cc.RedT.inGame.dialog.resetSizeDialog(this.node);
-    },
-	setMusic: function(){
+	onEnable: function ()
+	{
+		this.node.runAction(cc.RedT.inGame.dialog.actionShow);
+	},
+	onDisable: function ()
+	{
+		cc.RedT.inGame.dialog.resetSizeDialog(this.node);
+	},
+	setMusic: function ()
+	{
 		var check = localStorage.getItem('SOUND_GAME');
-		if(check == null){
+		if (check == null)
+		{
 			cc.RedT.setSoundGame(true);
-		}else{
-			if (cc.RedT.isSoundGame()) {
+		} else
+		{
+			if (cc.RedT.isSoundGame())
+			{
 				cc.RedT.IS_SOUND = true;
-			}else{
+			} else
+			{
 				cc.RedT.IS_SOUND = false;
 			}
 		}
 	},
-	OnChangerNhacNen: function() {
+	OnChangerNhacNen: function ()
+	{
 		cc.RedT.setSoundBackground(this.NhacNen.isChecked);
-		if (this.NhacNen.isChecked) {
+		if (this.NhacNen.isChecked)
+		{
 			cc.RedT.audio.bg.play();
-			let t = setInterval(function(){
+			let t = setInterval(function ()
+			{
 				console.log(cc.RedT.audio.bg.clip.loaded);
-				if(cc.RedT.audio.bg.clip.loaded){
+				if (cc.RedT.audio.bg.clip.loaded)
+				{
 					clearInterval(t);
 					cc.RedT.audio.bg.play();
-				}	
-			}.bind(this),100);
-		}else{
+				}
+			}.bind(this), 100);
+		} else
+		{
 			cc.RedT.audio.bg.stop();
 		}
 	},
-	OnChangerNhacGame: function() {
+	OnChangerNhacGame: function ()
+	{
 		cc.RedT.setSoundGame(this.NhacGame.isChecked);
-		if (this.NhacGame.isChecked) {
+		if (this.NhacGame.isChecked)
+		{
 			cc.RedT.IS_SOUND = true;
-		}else{
+		} else
+		{
 			cc.RedT.IS_SOUND = false;
 		}
 	},
-	OnSignOutClick: function() {
-		cc.RedT.inGame.notice.show({title: "", text: "Xác nhận hành động.\nHành động thực hiện đăng xuất khỏi tài khoản này?", button:{type: "sign_out", text: ""}})
+	OnChangerLanguage: function (data)
+	{
+		console.log(data);
+	},
+	OnSignOutClick: function ()
+	{
+		cc.RedT.inGame.notice.show({ title: "", text: "Confirm.\nDo you want to sign out？", button: { type: "sign_out", text: "" } })
 	},
 });
